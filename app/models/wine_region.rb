2e5126abf3a9712b8b172cb52b9942525cae03ce
Anna Krupp <cherchezlafemme@users.noreply.github.com>
2016-09-03 11:47:26 -0700

    Fix the typo in WIneRegion model

--- app/models/wine_region.rb
+++ app/models/wine_region.rb
@@ -1,7 +1,7 @@
 class WineRegion < ApplicationRecord
   validates_presence_of :name
   belongs_to :country
-  had_many :appellations
+  has_many :appellations
   belongs_to :state, optional: true
   # has_and_belongs_to_many :varietals
 end
