59868d8740185c3ce61d1090fa3fb91b4ba28fb6
Anna Krupp <cherchezlafemme@users.noreply.github.com>
2016-09-03 14:20:18 -0700

    Create appellations and varietlas join table. Update appelation, varietal models to reflect new associations (#11)

--- app/models/appellation.rb
+++ app/models/appellation.rb
@@ -2,5 +2,5 @@ class Appellation < ApplicationRecord
   validates_presence_of :name
   belongs_to :wine_region, optional: true
   belongs_to :state, optional: true
-  # has_and_belongs_to_many :varietals
+  has_and_belongs_to_many :varietals
 end
