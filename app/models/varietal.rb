7cc11f83a98adf5961ed98b538001f107a0201c4
Anna Krupp <cherchezlafemme@users.noreply.github.com>
2016-09-03 12:09:02 -0700

    Create countries and varietals join table (#9)

--- app/models/varietal.rb
+++ app/models/varietal.rb
@@ -2,5 +2,6 @@ class Varietal < ApplicationRecord
   validates_presence_of :name
   validates_inclusion_of :is_black, in: [true, false]
   has_and_belongs_to_many :blends
+  has_and_belongs_to_many :countries
   has_and_belongs_to_many :wine_regions
 end
