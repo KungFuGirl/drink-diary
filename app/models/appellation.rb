131eebf292d0d10343976683ec06b527224b375a
Anna Krupp <cherchezlafemme@users.noreply.github.com>
2016-09-02 22:42:41 -0700

    Create appellation model. Update state and wine region models to add associations to the appelation model

--- app/models/appellation.rb
+++ app/models/appellation.rb
@@ -1,7 +1,6 @@
-class WineRegion < ApplicationRecord
+class Appellation < ApplicationRecord
   validates_presence_of :name
-  belongs_to :country
+  belongs_to :wine_region
   belongs_to :state
-  had_many :appellations
   # has_and_belongs_to_many :varietals
 end
