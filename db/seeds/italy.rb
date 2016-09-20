3186a3a564bfc56369be1da241a5d95873b52136
Anna Krupp <cherchezlafemme@users.noreply.github.com>
2016-09-20 16:35:01 -0700

    Add italian wine regions to italy.rb in seeds

--- db/seeds/italy.rb
+++ db/seeds/italy.rb
@@ -1,9 +1,28 @@
 italy = Country.find_by( name: "Italy" )
 
 italy_wine_data = [
-
-
-
-
-
+  "Abruzzo",
+  "Basilicata",
+  "Calabria",
+  "Campania",
+  "Emilia Romagna",
+  "Friuli Venezia Giulia",
+  "Lazio",
+  "Liguria",
+  "Lombardia",
+  "Marche",
+  "Molise",
+  "Piemonte il Monferrato",
+  "Piemonte il Nord",
+  "Piemonte l’Astigiano",
+  "Piemonte le Langhe",
+  "Puglia",
+  "Sardegna",
+  "Sicilia",
+  "Toscana",
+  "Toscana 'Il cuore antico'",
+  "Trentino Alto Adige",
+  "Umbria",
+  "Valle d’Aosta",
+  "Veneto"
 ]
