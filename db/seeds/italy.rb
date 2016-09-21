9e1effc225ae0f54b43d077a960e80ffef65a390
Anna Krupp <cherchezlafemme@users.noreply.github.com>
2016-09-20 18:13:56 -0700

    Add seed wine data for Basilicata, Calabria, Campania wine regions

--- db/seeds/italy.rb
+++ db/seeds/italy.rb
@@ -15,29 +15,156 @@ italy_wine_data = [
     ]
    },
     
-  {"Basilicata": []},
-  {"Calabria": []},
-  {"Campania": []},
-  {"Emilia Romagna": []},
-  {"Friuli Venezia Giulia": []},
-  {"Lazio": []},
-  {"Liguria": []},
-  {"Lombardia": []},
-  {"Marche": []},
-  {"Molise": []},
-  {"Piemonte il Monferrato": []},
-  {"Piemonte il Nord": []},
-  {"Piemonte l’Astigiano": []},
-  {"Piemonte le Langhe": []},
-  {"Puglia": []},
-  {"Sardegna": []},
-  {"Sicilia": []},
-  {"Toscana": []},
-  {"Toscana 'Il cuore antico'": []},
-  {"Trentino Alto Adige": []},
-  {"Umbria": []},
-  {"Valle d’Aosta": []},
-  {"Veneto": []}
+  {"Basilicata": [
+    { "D.O.C.G.": ["Aglianico del Vulture Superiore"],
+      "D.O.C.": [
+        "Aglianico del Vulture",
+        "Grottino de Roccanova",
+        "Matera",
+        "Terre dell'Alta Val d'Agri"
+        ] }
+    ]},
+
+  {"Calabria": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [
+        "Bivongi",
+        "Ciro",
+        "Greco di Bianco",
+        "Lamezia",
+        "Melissa",
+        "S.Anna di Isola Capo Rizzuto",
+        "Savuto",
+        "Scavigna",
+        "Terre di Cosenza"
+        ] }
+    ]},
+
+  {"Campania": [
+    { "D.O.C.G.": [
+      "Aglianico del Taburno",
+      "Fiano di Avellino",
+      "Greco di Tufo",
+      "Taurasi"
+      ],
+      "D.O.C.": [
+        "Aversa",
+        "Campi Flegrei",
+        "Capri",
+        "Casavecchia di Pontelatone",
+        "Castel San Lorenzo",
+        "Cilento",
+        "Costa d'Amalfi",
+        "Falanghina del Sannio",
+        "Falerno del Massico",
+        "Galluccio",
+        "Irpinia",
+        "Ischia",
+        "Penisola Sorrentina",
+        "Sannio",
+        "Vesuvio"
+        ] }
+    ]},
+
+  {"Emilia Romagna": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Friuli Venezia Giulia": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Lazio": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Liguria": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Lombardia": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Marche": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Molise": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Piemonte il Monferrato": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Piemonte il Nord": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Piemonte l’Astigiano": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Piemonte le Langhe": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Puglia": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Sardegna": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Sicilia": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Toscana": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Toscana 'Il cuore antico'": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Trentino Alto Adige": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Umbria": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Valle d’Aosta": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]},
+
+  {"Veneto": [
+    { "D.O.C.G.": [],
+      "D.O.C.": [] }
+    ]}
 ]
 
 italy_wine_data.each do |wine_data|
