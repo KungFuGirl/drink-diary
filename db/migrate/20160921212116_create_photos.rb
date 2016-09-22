fd5de1a49d0ae25a8f7846a4e9b667acca97be63
bambery <lwszolek@gmail.com>
2016-09-21 17:25:48 -0700

    add soda and photo models

---
+++ db/migrate/20160921212116_create_photos.rb
@@ -0,0 +1,12 @@
+class CreatePhotos < ActiveRecord::Migration[5.0]
+  def change
+    create_table :photos do |t|
+      t.references :user, foreign_key :true
+      t.references :soda, foreign_key :true
+      t.attachment :photo, :null => false
+      t.boolean :is_private
+      
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20160921214741_create_sodas.rb
@@ -0,0 +1,18 @@
+class CreateSodas < ActiveRecord::Migration[5.0]
+  def change
+    create_table :sodas do |t|
+      t.references :user, foreign_key: true
+      t.references :soda, foreign_key: true
+      t.string :name
+      t.string :brand
+      t.string :origin_data
+      t.boolean :is_diet
+      t.boolean :is_caffeinated
+      t.string :color
+      t.references :users, column: :creator_id
+      t.references :users, column: :last_editor_id
+
+      t.timestamps
+    end
+  end
+end
