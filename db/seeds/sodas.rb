32e6d3365956e7c460550fbcc3ea607251c5fd2e
bambery <lwszolek@gmail.com>
2016-09-23 15:55:43 -0700

    adding flavor and soda data

---
+++ db/seeds/sodas.rb
@@ -0,0 +1,19 @@
+soda_data = {
+  "coca-cola":  { #parent-company
+    "coca-cola": { #brand
+      "Coca-cola": { #name
+        "flavors": [
+          "cola"
+        ],
+        "ingredients": [ 
+          "carbonated water",
+          "high fructose corn syrup",
+          "caramel color",
+          "phosphoric acid",
+          "natural flavors",
+          "caffeine"
+        ],
+      } # /name
+    } # /brand 
+  } # /parent-company
+} # /soda_data
